3d0c4cd14baab2472f6445ce8f937673063735d6
flin-7 <flin.7@icloud.com>
2021-06-04 00:07:38 -0700

    Profile View - Part 1 - Name Section

--- DubDubGrub/DubDubGrub/ProfileView.swift
+++ DubDubGrub/DubDubGrub/ProfileView.swift
@@ -8,13 +8,61 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State private var firstName    = ""
+    @State private var lastName     = ""
+    @State private var companyName  = ""
+    @State private var bio          = ""
+    
     var body: some View {
-        Text("Profile")
+        VStack {
+            ZStack {
+                Color(.secondarySystemBackground)
+                    .frame(height: 130)
+                    .cornerRadius(12)
+                    .padding(.horizontal)
+                
+                HStack(spacing: 16) {
+                    ZStack {
+                        AvatarView(size: 84)
+                            
+                        Image(systemName: "square.and.pencil")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 14, height: 14)
+                            .foregroundColor(.white)
+                            .offset(y: 30)
+                    }
+                    .padding(.leading, 12)
+                    
+                    VStack(spacing: 1) {
+                        TextField("First Name", text: $firstName)
+                            .font(.system(size: 32, weight: .bold))
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.75)
+                        
+                        TextField("Last Name", text: $lastName)
+                            .font(.system(size: 32, weight: .bold))
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.75)
+                        
+                        TextField("Company Name", text: $companyName)
+                    }
+                    .padding(.trailing, 16)
+                }
+                .padding()
+            }
+            
+            Spacer()
+        }
+        .navigationTitle("Profile")
     }
 }
 
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileView()
+        NavigationView {
+            ProfileView()
+        }
     }
 }
