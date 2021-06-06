4a7319cde21d1e50b3b54147186ccf228c2435d4
flin-7 <flin.7@icloud.com>
2021-06-06 16:24:31 -0700

    Profile View - Part 2 - Bio Character Count View

--- DubDubGrub/DubDubGrub/ProfileView.swift
+++ DubDubGrub/DubDubGrub/ProfileView.swift
@@ -53,7 +53,41 @@ struct ProfileView: View {
                 .padding()
             }
             
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Bio: ")
+                    .font(.callout)
+                    .foregroundColor(.secondary)
+                +
+                    Text("\(100 - bio.count)")
+                    .bold()
+                    .font(.callout)
+                    .foregroundColor(bio.count <= 100 ? .brandPrimary : Color(.systemPink))
+                +
+                Text(" Characters Remain")
+                    .font(.callout)
+                    .foregroundColor(.secondary)
+                
+                TextEditor(text: $bio)
+                    .frame(height: 100)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 8)
+                            .stroke(Color.secondary, lineWidth: 1)
+                    )
+            }
+            .padding(.horizontal, 20)
+            
             Spacer()
+            
+            Button {
+                
+            } label: {
+                Text("Create Profile")
+                    .bold()
+                    .frame(width: 280, height: 44)
+                    .background(Color.brandPrimary)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
         }
         .navigationTitle("Profile")
     }
