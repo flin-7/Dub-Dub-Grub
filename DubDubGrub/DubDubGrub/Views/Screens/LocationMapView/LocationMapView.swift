6cdc14423e1e72f71cdb3bd60f33bdefbc60aaeb
Felix Lin <flin.7@icloud.com>
2021-06-20 15:41:38 -0700

    MVVM Setup

--- DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
+++ DubDubGrub/DubDubGrub/Views/Screens/LocationMapView/LocationMapView.swift
@@ -9,32 +9,22 @@ import SwiftUI
 import MapKit
 
 struct LocationMapView: View {
-    
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054),
-                                                   span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-    @State private var alertItem: AlertItem?
+    @StateObject private var viewModel = LocationMapViewModel()
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $region).ignoresSafeArea()
+            Map(coordinateRegion: $viewModel.region).ignoresSafeArea()
             
             VStack {
                 LogoView().shadow(radius: 10)
                 Spacer()
             }
         }
-        .alert(item: $alertItem, content: { alertItem in
+        .alert(item: $viewModel.alertItem, content: { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         })
         .onAppear {
-            CloudKitManager.getLocations { result in
-                switch result {
-                case .success(let locations):
-                    print(locations)
-                case .failure(_):
-                    alertItem = AlertContext.unableToGetLocations
-                }
-            }
+            viewModel.getLocations()
         }
     }
 }
